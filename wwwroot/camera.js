f815364eb2ca8846022be2d95a257dc26d8ea0bc
TatarPrincess <ksusha.bukovskaya@gmail.com>
2024-12-10 17:39:43 +0300

    Initial version

---
+++ wwwroot/camera.js
@@ -0,0 +1,10 @@
+﻿export async function startCamera()
+{
+    const videoElement = document.getElementById('videoElement');
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        videoElement.srcObject = stream;
+    } else {
+        console.error("Camera not supported on this device.");
+    }
+};
